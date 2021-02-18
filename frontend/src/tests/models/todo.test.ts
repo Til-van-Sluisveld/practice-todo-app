2ae71eac7d1199c86644ca826e81c8100557c159
Til-van_sluisveld <Tilvansluisveld@gmail.com>
2021-02-18 16:33:54 +0100

    Added test fro creating new user

--- frontend/src/tests/models/todo.test.ts
+++ frontend/src/tests/models/todo.test.ts
@@ -1,4 +1,5 @@
 import ToDo from "../../store/models/todo";
+import User from "../../store/models/user";
 
 const newToDo = (title: string, description: string, finished: boolean) =>
   new ToDo({ title, description, finished });
@@ -9,7 +10,19 @@ test("creating a new todo item", () => {
       title: "first",
       description: "first item",
       finished: false,
-      userId: null,
+    })
+  );
+});
+
+const newUser = (firstName: string, lastName: string, email: string) =>
+  new User({ firstName, lastName, email });
+
+test("creating a new user", () => {
+  expect(newUser("James", "McGill", "SlippinJimmy@HHM.com")).toEqual(
+    expect.objectContaining({
+      firstName: "James",
+      lastName: "McGill",
+      email: "SlippinJimmy@HHM.com",
     })
   );
 });
